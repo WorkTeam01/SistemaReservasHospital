f2ac39ae20e39e3483596d93b1ae26f28453292c
Jandres <sc.jose.meneces.l@upds.net.bo>
2025-12-07 08:45:22 -0400

    Dejar de rastrear config/config.php y agregarlo a gitignore

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Definir la URL base del proyecto usando la variable de entorno o un fallback
-define('URL_BASE', env('BASE_URL'));
-
-// Otras configuraciones globales
-date_default_timezone_set('America/La_Paz');
